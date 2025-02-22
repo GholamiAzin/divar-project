576e6b9c325a776f16010cf61f3a9e6ba3f12e4a
GholamiAzin <gholamiazin73@gamil.com>
2025-02-22 16:25:08 +0330

    feat: enhance authentication middleware to handle token refresh and improve route protection but still have problems!!!!

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { createSession } from "./lib/sessions";
 
 // 1. Specify protected and public routes
 const protectedRoutes = "/dashboard";
 const publicRoutes = "/";
-
+const BASE_URL = process.env.BASE_URL;
 export default async function middleware(req: NextRequest) {
+  debugger
   // 2. Check if the current route is protected or public
   const path = req.nextUrl.pathname;
   const isProtectedRoute = path.startsWith(protectedRoutes);
@@ -13,17 +15,40 @@ export default async function middleware(req: NextRequest) {
 
   // 3. Decrypt the session from the cookie
   const accessToken = (await cookies()).get("accessToken")?.value;
+  const refreshToken = (await cookies()).get("refreshToken")?.value;
+  const isLogin = accessToken && refreshToken;
+  const isLogout = !accessToken && !refreshToken;
+  const needToRefresh = !accessToken && refreshToken;
+
+  if (needToRefresh) {
+    try {
+      const newResult = await fetch(`${BASE_URL}/auth/refresh`, {
+        method: "post",
+        body: JSON.stringify({refreshToken: refreshToken}),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      const newData = await newResult.json();
+      if (newResult.ok) {
+        await createSession({
+          accessToken: newData.accessToken,
+          refreshToken: newData.refreshToken,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
+  }
 
   // 4. Redirect to /login if the user is not authenticated
-  if (isProtectedRoute && !accessToken) {
+  if (isProtectedRoute && isLogout) {
     return NextResponse.redirect(new URL("/auth/login", req.nextUrl));
   }
 
   // 5. Redirect to /dashboard if the user is authenticated
-  if (
-    isPublicRoute &&
-    accessToken
-  ) {
+  if (isPublicRoute && isLogin) {
     return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
   }
 
