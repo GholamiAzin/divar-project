a58425e997c3143edbcf63db4ee4bce3bb514ed5
GholamiAzin <gholamiazin73@gamil.com>
2025-03-05 17:31:38 +0330

    feat: add dashboard layout components including header, drawer, and search functionality

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,22 @@
+import DrawerHeader from "@/components/dashboard-layout/drawer-header";
+import DashboardHeader from "@/components/dashboard-layout/header";
+import MiniDrawer from "@/components/dashboard-layout/side-bar";
+import { Box } from "@mui/material";
+
 const DashboradLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div>{children}</div>;
+  return (
+    <Box sx={{ display: "flex" }}>
+      <DashboardHeader />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <MiniDrawer/>
+        <DrawerHeader />
+      </Box>
+    </Box>
+  );
 };
 
-export default DashboradLayout
+export default DashboradLayout;
