ffcf56883abdaea7d522b067a4638fe3264ce0e8
GholamiAzin <gholamiazin73@gamil.com>
2025-03-06 13:49:19 +0330

    feat: simplify MiniDrawer component and introduce SIDE_ITEMS constant for navigation

---
+++ src/components/dashboard-layout/constants.ts
@@ -0,0 +1,14 @@
+import { Mail } from "@mui/icons-material";
+
+export const SIDE_ITEMS = [
+  { href: "/dashboard/badges", Icon: Mail, text: "برچسب‌ها" },
+  { href: "/dashboard/brands", Icon: Mail, text: "برند" },
+  { href: "/dashboard/categories", Icon: Mail, text: "دسته‌بندی کالاها" },
+  { href: "/dashboard/cities", Icon: Mail, text: "شهرها" },
+  { href: "/dashboard/colors", Icon: Mail, text: "رنگ‌ها" },
+  { href: "/dashboard/features", Icon: Mail, text: "ویژگی‌ها" },
+  { href: "/dashboard/products", Icon: Mail, text: "محصولات" },
+  { href: "/dashboard/users", Icon: Mail, text: "کاربران" },
+  { href: "/dashboard/sellers", Icon: Mail, text: "فروشندگان" },
+  { href: "/dashboard/orders", Icon: Mail, text: "سفارشات" },
+];
